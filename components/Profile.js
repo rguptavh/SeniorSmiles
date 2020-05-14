50634337a9c99dfb45e2de7c24d4d86c2aaff425
rguptavh <rguptavh119@gmail.com>
2020-05-14 00:49:32 -0500

    superhuman badge

--- components/Profile.js
+++ components/Profile.js
@@ -27,7 +27,7 @@ export default class Login extends React.Component {
     keyboard: false,
     hours: '50',
     minutes: '47',
-    badges: [{name: 'Numero Uno',},{name: 'Workhorse Bronze',},{name: 'Workhorse Silver',},{name: 'Workhorse Gold',}, {name: 'Wingman Bronze',},{name: 'Wingman Silver',},{name: 'Wingman Gold',}],
+    badges: [{name: 'Numero Uno',},{name: 'Workhorse Bronze',},{name: 'Workhorse Silver',},{name: 'Workhorse Gold',}, {name: 'Wingman Bronze',},{name: 'Wingman Silver',},{name: 'Wingman Gold',},{name: 'Superhuman',}],
     logs: global.logs,
     hours: global.hours,
     minutes: global.minutes
@@ -362,6 +362,28 @@ export default class Login extends React.Component {
     else{
       return (
         <TouchableOpacity onPress={() => Alert.alert(item.name, 'Help 50 people to receive this badge!')}>
+
+          <Image style={{ height: '100%', width:entireScreenHeight/5.75 }} source={require('../assets/round.png')} resizeMode='contain' >
+          </Image>
+        </TouchableOpacity>
+
+      );
+    }
+
+  }
+  if (item.name == 'Superhuman'){
+    if(global.peoplehelped>=500){
+      return (
+        <TouchableOpacity onPress={() => Alert.alert(item.name, 'You already have this badge\nRequirement: Help 500 people')}>
+          <Image style={{ height: '100%', width:entireScreenHeight/5.75, marginLeft:wid*10 }} source={require('../assets/super.png')} resizeMode='contain' >
+          </Image>
+        </TouchableOpacity>
+
+      );
+    }
+    else{
+      return (
+        <TouchableOpacity onPress={() => Alert.alert(item.name, 'Help 500 people to receive this badge!')}>
 
           <Image style={{ height: '100%', width:entireScreenHeight/5.75 }} source={require('../assets/round.png')} resizeMode='contain' >
           </Image>
